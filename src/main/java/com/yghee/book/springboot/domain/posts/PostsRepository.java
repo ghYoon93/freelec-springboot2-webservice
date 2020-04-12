4730c21948204f454a1ef88117ac8154cea8f26c
ghYoon93 <gh.yoon93@gmail.com>
2020-04-12 19:46:08 +0900

    전체 조회 화면 생성

--- src/main/java/com/yghee/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/yghee/book/springboot/domain/posts/PostsRepository.java
@@ -1,8 +1,12 @@
 package com.yghee.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /* DB 레이어 접근자 */
 public interface PostsRepository extends JpaRepository<Posts, Long> {
-
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/yghee/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/yghee/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.yghee.book.springboot.service.posts;
 
 import com.yghee.book.springboot.domain.posts.Posts;
 import com.yghee.book.springboot.domain.posts.PostsRepository;
+import com.yghee.book.springboot.web.dto.PostsListResponseDto;
 import com.yghee.book.springboot.web.dto.PostsResponseDto;
 import com.yghee.book.springboot.web.dto.PostsSaveRequestDto;
 import com.yghee.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,13 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id= " + id));
                 return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
 
 }
