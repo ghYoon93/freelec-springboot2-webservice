a82a116ad32efa92c6688027fca06c0024bfdd8c
ghYoon93 <gh.yoon93@gmail.com>
2020-04-12 20:29:03 +0900

    게시글 수정, 삭제 화면 생성

--- src/main/java/com/yghee/book/springboot/web/PostsApiController.java
+++ src/main/java/com/yghee/book/springboot/web/PostsApiController.java
@@ -18,6 +18,7 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
+
     @PutMapping("/api/v1/posts/{id}")
     public Long update (@PathVariable Long id,
                         @RequestBody PostsUpdateRequestDto requestDto) {
@@ -27,6 +28,12 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById (@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete (@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 
 }
