ffd1a16ecc4987a3b9cfdff8191bcc8b6f53df21
ghYoon93 <gh.yoon93@gmail.com>
2020-04-13 18:46:18 +0900

    구글 로그인 연동

--- src/main/java/com/yghee/book/springboot/web/IndexController.java
+++ src/main/java/com/yghee/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.yghee.book.springboot.web;
 
+import com.yghee.book.springboot.config.auth.dto.SessionUser;
 import com.yghee.book.springboot.service.posts.PostsService;
 import com.yghee.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {  // 서버 템플릿 엔진에서 사용할 수 있는 객체 저장
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user"); // CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성 즉, 로그인 성공시 세션에서 값을 가져올 수 있음
+        if(user != null) {  // 세션에 저장된 값이 있을 때만 model에 userName으로 등록
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
     @GetMapping("/posts/save")
