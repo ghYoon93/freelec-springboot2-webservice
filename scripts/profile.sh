b337560cecd67d9c381181e38add88331955db0d
ghYoon93 <gh.yoon93@gmail.com>
2020-04-16 20:35:48 +0900

    10.4 무중단 배포 스크립트 테스트6

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -26,7 +26,7 @@ function find_idle_profile()
 function find_idle_port()
 {
     IDLE_PROFILE=$(find_idle_profile)
-
+    echo "> idle_profile: ${IDLE_PROFILE}"
     if [ ${IDLE_PROFILE} == real1 ]
     then
       echo "8081"
